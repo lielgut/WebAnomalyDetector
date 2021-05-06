198eef31db9d8e9a218f620bcfbc5346571199ba
Liel <69732709+lielgut@users.noreply.github.com>
2021-05-07 02:43:43 +0300

    add mongoDB to project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
-}));
+  }));
 const bodyParser = require('body-parser')
 const querystring = require('querystring')
 const anomDet = require('./algorithm/AnomalyDetector.js');
@@ -13,142 +13,165 @@ const SimpleAnomalyDetector = anomDet.SimpleAnomalyDetector;
 const HybridAnomalyDetector = anomDet.HybridAnomalyDetector;
 const fs = require('fs');
 const moment = require("moment");
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/modelsDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-if (!fs.existsSync('./models/')) {
-    fs.mkdirSync('./models/');
-}
-
-if (!fs.existsSync('./detectors/')) {
-    fs.mkdirSync('./detectors/');
-}
-
-// load models and data from files
-var models = [];
-var detectors = [];
-fs.readdirSync('./models/').forEach(file => {
-    let split = file.split('.');
-    if (split[split.length - 1] == 'json') {
-        let model = JSON.parse(fs.readFileSync('./models/' + file, 'utf8'));
-        models[model.model_id] = model;
-        let detData = JSON.parse(fs.readFileSync('./detectors/' + file, 'utf8'));
-        let det;
-        if (detData.type == 'regression') {
-            det = new SimpleAnomalyDetector();
-        } else if (detData.type == 'hybrid') {
-            det = new HybridAnomalyDetector();
-        }
-        det.cf = detData.cf;
-        det.threshold = detData.threshold;
-        detectors[model.model_id] = det;
-    }
+const modelSchema = new mongoose.Schema ({
+    model_id: Number,
+    upload_time: String,
+    status: String
 });
+const Model = mongoose.model("Model", modelSchema);
 
-app.get("/", function (request, response) {
+const detectorSchema = new mongoose.Schema ({
+    model_id: Number,
+    cf: [{
+            feature1: String,
+            feature2: String,
+            correlation: Number,
+            lin_reg: {a: Number, b: Number},
+            threshold: Number,
+            cx: Number,
+            cy: Number
+        }],
+    threshold: Number,
+    type: String
+});
+const Detector = mongoose.model("Detector", detectorSchema);
+
+app.get("/", function(request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/style.css", function (request, response) {
+app.get("/style.css", function(request, response) {
     response.sendFile(__dirname + "/style.css");
 });
 
-app.get("/index.js", function (request, response) {
+app.get("/index.js", function(request, response) {
     response.sendFile(__dirname + "/index.js");
 });
 
 app.post("/api/model", function (request, response) {
 
-    let data = request.body.train_data;
-    let id = models.length;
-    let currTime = moment().format();
-    let model = { model_id: models.length, upload_time: currTime, status: 'pending' };
-    response.send(JSON.stringify(model));
-    models.push(model);
-
-
-    let t = new TimeSeries(data);
-
-    let modelType = request.query.model_type;
-
-    let det;
-    if (modelType == 'regression') {
-        det = new SimpleAnomalyDetector();
-    } else if (modelType == 'hybrid') {
-        det = new HybridAnomalyDetector();
-    } else {
-        // TODO error
-    }
-    detectors.push(det);
-    det.learnNormal(t);
-    model.status = 'ready';
-
-    let detData = { cf: det.cf, threshold: det.threshold, type: modelType };
-    fs.writeFileSync('./detectors/' + id + '.json', JSON.stringify(detData), err => {
-        if (err) {
-            // TODO something
+    Model.countDocuments({}, (err, count) => {
+        if(err) {
+            console.log("error in countDocuments");
         }
-        console.log("saved detector file");
-    })
+        else {
+            let data = request.body.train_data;
+            let currTime = moment().format();
+            let id = count + 1;
+            let model = { model_id: id, upload_time: currTime, status: 'pending' };
+            response.send(JSON.stringify(model));
+            const modelEntry = new Model(model);
+            modelEntry.save().then(() => {
+                let t = new TimeSeries(data);
 
-    fs.writeFileSync('./models/' + id + '.json', JSON.stringify(model), err => {
-        if (err) {
-            // TODO something
+                let modelType = request.query.model_type;
+                
+                let det;
+                if(modelType == 'regression') {
+                    det = new SimpleAnomalyDetector();
+                } else if(modelType == 'hybrid') {
+                    det = new HybridAnomalyDetector();
+                } else {
+                    // TODO error
+                }
+                
+                det.learnNormal(t);
+                Model.updateOne({ model_id: id }, { $set: {status: 'ready'}}).then(() => {
+                    let detData = {model_id: id, cf: det.cf, threshold: det.threshold, type: modelType};
+                    let detector = new Detector(detData);
+                    detector.save();
+                });
+
+            });
         }
-        console.log("saved model file");
     });
-
 });
 
 app.get("/api/model", function (request, response) {
-    let model_id = request.query.model_id;
-    let model = models[model_id];
-    if (model != undefined) {
-        response.send(JSON.stringify(model));
-    } else {
-        response.status(406).send('found no model with given id');
-    }
+    let id = request.query.model_id;
+    Model.findOne({model_id: id}, {_id: 0, model_id: 1, upload_time: 1, status: 1}, (err, model) => {
+        if(err) {
+            console.log("error deleting model " + id);
+        } else {
+            if(model != null) {
+                response.send(JSON.stringify(model));
+            } else {
+                response.status(406).send('found no model with given id'); 
+            }
+        }
+    });
 });
 
 app.delete("/api/model", function (request, response) {
-    let model_id = request.query.model_id;
-    if (models[model_id] != undefined) {
-        models[model_id] = undefined;
-        detectors[model_id] = undefined;
-        response.send();
-        fs.unlinkSync('./models/' + model_id + '.json');
-        fs.unlinkSync('./detectors/' + model_id + '.json');
-    } else {
-        response.status(406).send('found no model with given id');
-    }
+    let id = request.query.model_id;
+    Model.deleteOne({model_id: id}, (err, result) => {
+        if(err) {
+            // ...
+        } else {
+            if(result.deletedCount != 0) {
+                response.send();
+                Detector.deleteOne({model_id: id}, (err) => {
+                    if(err) {
+                        console.log("error deleting detector " + id);           
+                    }
+                });
+            } else {
+                response.status(406).send('found no model with given id'); 
+            }
+        }
+    });
 });
 
 app.get("/api/models", function (request, response) {
-    let m = [];
-    for (let i = 0; i < models.length; i++) {
-        if (models[i] != undefined) {
-            m.push(models[i]);
+    Model.find({}, {_id: 0, model_id: 1, upload_time: 1, status: 1}, (err, models) => {
+        if(err) {
+            console.log("error getting models");
+        } else {
+            response.send(JSON.stringify(models));
         }
-    }
-    response.send(JSON.stringify(m));
+    });
 });
 
 app.post("/api/anomaly", function (request, response) {
-    let model_id = request.query.model_id;
-    let det = detectors[model_id];
-    if (det == undefined) {
-        response.status(406).send('found no model with given id');
-    }
-    else {
-        if (models[model_id].status == 'ready') {
-            let data = request.body.predict_data;
-            let t = new TimeSeries(data);
-            response.send(JSON.stringify({ anomalies: det.detect(t), reason: '??' }));
+    let id = request.query.model_id;
+    
+    Detector.findOne({model_id: id}, (err, detInfo) => {
+        if(err) {
+            console.log("error getting detector " + id);
+        } else {
+            if(detInfo != null) {
+                Model.findOne({model_id: id}, (err, model) => {
+                    if(err) {
+                        console.log("error getting model " + id);
+                    } else {
+                        if(model.status == 'ready') {
+                            let data = request.body.predict_data;
+                            let t = new TimeSeries(data);
+                            let det;
+                            if(detInfo.type == 'regression') {
+                                det = new SimpleAnomalyDetector();
+                            } else if(detInfo.type == 'hybrid') {
+                                det = new HybridAnomalyDetector();
+                            }
+                            det.cf = detInfo.cf;
+                            det.threshold = detInfo.threshold;
+                            response.send(JSON.stringify({anomalies: det.detect(t), reason: undefined}));
+                        }
+                        else {
+                            response.redirect(303, '/api/model?model_id=' + id);
+                        }
+                    }
+                });
+            } else {
+                response.status(406).send('found no model with given id'); 
+            }
         }
-        else {
-            response.redirect(303, '/api/model?model_id=' + model_id);
-        }
-    }
+    });
 });
 
-app.listen(9876, function () {
+app.listen(9876, function() {
     console.log("Server started on port 9876.")
 })
