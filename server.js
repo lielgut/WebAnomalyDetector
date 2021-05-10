44792b83e4c7475e7fff128c58751f1af0d540a6
Eyal Michon <eyalmichon1@gmail.com>
2021-05-10 23:46:39 +0300

    Add logo and favicon

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
     extended: true,
     limit: '50mb'
-  }));
+}));
 const bodyParser = require('body-parser')
 const querystring = require('querystring')
 const anomDet = require('./algorithm/AnomalyDetector.js');
@@ -15,47 +15,54 @@ const HybridAnomalyDetector = anomDet.HybridAnomalyDetector;
 const fs = require('fs');
 const moment = require("moment");
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/modelsDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/modelsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const modelSchema = new mongoose.Schema ({
+const modelSchema = new mongoose.Schema({
     model_id: Number,
     upload_time: String,
     status: String
 });
 const Model = mongoose.model("Model", modelSchema);
 
-const detectorSchema = new mongoose.Schema ({
+const detectorSchema = new mongoose.Schema({
     model_id: Number,
     cf: [{
-            feature1: String,
-            feature2: String,
-            correlation: Number,
-            lin_reg: {a: Number, b: Number},
-            threshold: Number,
-            cx: Number,
-            cy: Number
-        }],
+        feature1: String,
+        feature2: String,
+        correlation: Number,
+        lin_reg: { a: Number, b: Number },
+        threshold: Number,
+        cx: Number,
+        cy: Number
+    }],
     threshold: Number,
     type: String
 });
 const Detector = mongoose.model("Detector", detectorSchema);
 
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
     response.sendFile(__dirname + "/index.html");
 });
 
-app.get("/style.css", function(request, response) {
+app.get("/style.css", function (request, response) {
     response.sendFile(__dirname + "/style.css");
 });
 
-app.get("/index.js", function(request, response) {
+app.get("/favicon.png", function (request, response) {
+    response.sendFile(__dirname + "/favicon.png");
+});
+app.get("/logo.svg", function (request, response) {
+    response.sendFile(__dirname + "/logo.svg");
+});
+
+app.get("/index.js", function (request, response) {
     response.sendFile(__dirname + "/index.js");
 });
 
 app.post("/api/model", function (request, response) {
 
     Model.countDocuments({}, (err, count) => {
-        if(err) {
+        if (err) {
             console.log("error in countDocuments");
         }
         else {
@@ -69,19 +76,19 @@ app.post("/api/model", function (request, response) {
                 let t = new TimeSeries(data);
 
                 let modelType = request.query.model_type;
-                
+
                 let det;
-                if(modelType == 'regression') {
+                if (modelType == 'regression') {
                     det = new SimpleAnomalyDetector();
-                } else if(modelType == 'hybrid') {
+                } else if (modelType == 'hybrid') {
                     det = new HybridAnomalyDetector();
                 } else {
                     // TODO error
                 }
-                
+
                 det.learnNormal(t);
-                Model.updateOne({ model_id: id }, { $set: {status: 'ready'}}).then(() => {
-                    let detData = {model_id: id, cf: det.cf, threshold: det.threshold, type: modelType};
+                Model.updateOne({ model_id: id }, { $set: { status: 'ready' } }).then(() => {
+                    let detData = { model_id: id, cf: det.cf, threshold: det.threshold, type: modelType };
                     let detector = new Detector(detData);
                     detector.save();
                 });
@@ -93,14 +100,14 @@ app.post("/api/model", function (request, response) {
 
 app.get("/api/model", function (request, response) {
     let id = request.query.model_id;
-    Model.findOne({model_id: id}, {_id: 0, model_id: 1, upload_time: 1, status: 1}, (err, model) => {
-        if(err) {
+    Model.findOne({ model_id: id }, { _id: 0, model_id: 1, upload_time: 1, status: 1 }, (err, model) => {
+        if (err) {
             console.log("error deleting model " + id);
         } else {
-            if(model != null) {
+            if (model != null) {
                 response.send(JSON.stringify(model));
             } else {
-                response.status(406).send('found no model with given id'); 
+                response.status(406).send('found no model with given id');
             }
         }
     });
@@ -108,27 +115,27 @@ app.get("/api/model", function (request, response) {
 
 app.delete("/api/model", function (request, response) {
     let id = request.query.model_id;
-    Model.deleteOne({model_id: id}, (err, result) => {
-        if(err) {
+    Model.deleteOne({ model_id: id }, (err, result) => {
+        if (err) {
             // ...
         } else {
-            if(result.deletedCount != 0) {
+            if (result.deletedCount != 0) {
                 response.send();
-                Detector.deleteOne({model_id: id}, (err) => {
-                    if(err) {
-                        console.log("error deleting detector " + id);           
+                Detector.deleteOne({ model_id: id }, (err) => {
+                    if (err) {
+                        console.log("error deleting detector " + id);
                     }
                 });
             } else {
-                response.status(406).send('found no model with given id'); 
+                response.status(406).send('found no model with given id');
             }
         }
     });
 });
 
 app.get("/api/models", function (request, response) {
-    Model.find({}, {_id: 0, model_id: 1, upload_time: 1, status: 1}, (err, models) => {
-        if(err) {
+    Model.find({}, { _id: 0, model_id: 1, upload_time: 1, status: 1 }, (err, models) => {
+        if (err) {
             console.log("error getting models");
         } else {
             response.send(JSON.stringify(models));
@@ -138,23 +145,23 @@ app.get("/api/models", function (request, response) {
 
 app.post("/api/anomaly", function (request, response) {
     let id = request.query.model_id;
-    
-    Detector.findOne({model_id: id}, (err, detInfo) => {
-        if(err) {
+
+    Detector.findOne({ model_id: id }, (err, detInfo) => {
+        if (err) {
             console.log("error getting detector " + id);
         } else {
-            if(detInfo != null) {
-                Model.findOne({model_id: id}, (err, model) => {
-                    if(err) {
+            if (detInfo != null) {
+                Model.findOne({ model_id: id }, (err, model) => {
+                    if (err) {
                         console.log("error getting model " + id);
                     } else {
-                        if(model.status == 'ready') {
+                        if (model.status == 'ready') {
                             let data = request.body.predict_data;
                             let t = new TimeSeries(data);
                             let det;
-                            if(detInfo.type == 'regression') {
+                            if (detInfo.type == 'regression') {
                                 det = new SimpleAnomalyDetector();
-                            } else if(detInfo.type == 'hybrid') {
+                            } else if (detInfo.type == 'hybrid') {
                                 det = new HybridAnomalyDetector();
                             }
                             det.cf = detInfo.cf;
@@ -167,12 +174,12 @@ app.post("/api/anomaly", function (request, response) {
                     }
                 });
             } else {
-                response.status(406).send('found no model with given id'); 
+                response.status(406).send('found no model with given id');
             }
         }
     });
 });
 
-app.listen(9876, function() {
+app.listen(9876, function () {
     console.log("Server started on port 9876.")
 })
