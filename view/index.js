a710bd3de3ac43ab878498f782de273779269f76
Liel <69732709+lielgut@users.noreply.github.com>
2021-05-20 21:12:10 +0300

    add comments in index.js

--- view/index.js
+++ view/index.js
@@ -1,47 +1,43 @@
-var selectedModelID;
-var modelType;
-var loadedTrainFile;
-var loadedDetectFile;
-var loadedDetectData = {};
-var anomalyData = {};
-var selectedFeature;
-var graph;
-var corGraph;
+var selectedModelID; // currently selected model
+var modelType; // currently selected model type
+var loadedTrainFile; // name of file loaded into train drop-area
+var loadedDetectFile; // name of file loaded into detect drop-area
+var loadedDetectData = {}; // data loaded from detect file
+var anomalyData = {}; // anomaly data that is recieved
+var selectedFeature; // currently selected feature name from list
+var table; // the datatable
+var graph; // the graph for selected feature
+var corGraph; // the graph for the correlative feature
 
-// Check if the browser supports drag&drop ///////////////
-var supportsDragnDrop = function () {
-    var div = document.createElement('div');
-    return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-}();
+// dropArea animation when loading the page.
+$('.dropArea').addClass('dropAreaRefreshAnimation');
 
-if (supportsDragnDrop) {
-    // dropArea animation when refreshing the page.
-    $('.dropArea').addClass('dropAreaRefreshAnimation');
-}
-else {
-    alert("Please enable JavaScript.");
-}
-/////////////////////////////////////////////////////////
-
+// function for removing a model
 function removeModel(id) {
+    // change selected model if current selection was removed
     if (selectedModelID == id) {
         selectedModelID = undefined;
     }
+    // send a HTTP Delete request to the server
     $.ajax({
         url: '/api/model?model_id=' + id,
         type: 'DELETE',
         success: () => {
+            // if request was succesful, remove the model from table and notify user
             $("#tr" + id).remove();
             alertify.success("model " + id + " deleted succesfuly.");
         },
         error: () => {
+            // notify if the request failed
             alertify.error("error in deleting model");
         }
     });
 }
 
-function addModel(model) {
+// function for adding a new model
+function addModel(model) {    
     let id = model.model_id;
+    // append a new model row to the models table
     $("#modelTableBody").append("\
     <tr id=\"tr" + id + "\">\
     <td><input type=\"radio\" id=\"" + id + "\" name=\"radiobtn\"></td>\
@@ -50,27 +46,33 @@ function addModel(model) {
     <td><button type=\"button\" id=\"delete" + id + "\" class=\"btn btn-sm btn-danger\">X</button></td>\
     </tr>\
     ");
+    // add a click event for selecting the radio button for added model
     $("#" + id).click(() => {
         selectedModelID = id;
     });
+    // change selection to the new added model
     $("#" + id).click();
+    // add event for clicking the delete button 
     $("#delete" + id).click(() => {
         removeModel(id);
     });
 }
 
-var table;
-
+// function for creating the table
 function createTable() {
 
+    // delete previoulsy defined table if it exists
     if (table != undefined)
         table.destroy();
+    // clear any previous data in table
     $("#TableRows").html("");
 
+    // gets list of attributes
     let attrs = Object.keys(loadedDetectData);
     if (attrs.length == 0) {
         return;
     }
+    // get element of table headers
     let headers = $("#TableHeaders");
 
     let numRows = loadedDetectData[attrs[0]].length;
@@ -79,13 +81,16 @@ function createTable() {
     attrs.forEach(attr => {
         s += "<th class=\"thSticky\">" + attr + "</th>\n";
     });
+    // set headers in the html
     headers.html(s);
 
+    // create the data table
     table = $('#DetectTable').DataTable({
         ordering: false,
         searching: false
     });
 
+    // add all rows with data to the table
     for (let i = 0; i < numRows; i++) {
         let curRow = [];
         curRow.push(i);
@@ -95,32 +100,44 @@ function createTable() {
         table.row.add(curRow);
     }
 
+    // show the updated table
     table.draw();
 };
 
+// function for updating the list of selectable features
 function updateSelections() {
     let s = "";
+    // get list of attributes
     let attrs = Object.keys(loadedDetectData);
+    // add each attribute to the list of selectable features
     attrs.forEach(attr => {
         s += "<option id=\"select-" + attr + "\" value=\"" + attr + "\">" + attr + "</option>\n";
     });
+    // update html for the select element
     $("#featuresSelect").html(s);
 }
 
+// function for updating anomalies for selected feature on anomalies table and on graphs
 function updateSelectedAnomalies() {
 
     let s = "";
+    // if a feature was selected
     if (selectedFeature != undefined) {
 
+        // data of the anomaly to be presented on the graph
         let anomalyGraphData = [];
 
+        // get name of feature correlative to currently selected feature
         let corFeature = anomalyData.reason[selectedFeature];
+        // get data for currently selected feature
         let selectedData = loadedDetectData[selectedFeature];
-        let i = 0;
-
+        
+        // if a correlative feature exists
         if (corFeature != undefined) {
 
+            // if a column for the anomalies table doesn't exist in the html
             if ($("#anomaliesCol").length == 0) {
+                // add a new column for the anomalies table to the graphs div
                 $("#graphs-row").append("\
                 <div class=\"col-lg-2 col-md-12\" id=\"anomaliesCol\">\
                     <table id=\"anomaliesTable\" class=\"table table-hover table-bordered table-sm\">\
@@ -137,22 +154,31 @@ function updateSelectedAnomalies() {
                 ");
             }
 
-            let corAnomalyGraphData = [];
+            
+            // data for the correlative feature
             let corSelectedData = loadedDetectData[corFeature];
+            // anomaly data for the correlative feature graph
+            let corAnomalyGraphData = [];
 
+            let i = 0;
+            // iterate the ranges where anomalies existed for selected feature
             anomalyData.anomalies[selectedFeature].forEach(range => {
+                // add the anomaly range to the table
                 s += "<tr><td>" + range[0] + "</td><td>" + range[1] + "</td></tr>\n";
 
+                // fill the points that are not in range with undefined so they won't appear on graph
                 for (; i < range[0]; i++) {
                     anomalyGraphData.push(undefined);
                     corAnomalyGraphData.push(undefined);
                 }
+                // fill the points that are in range with the relevant data
                 for (; i < range[1]; i++) {
                     anomalyGraphData.push(selectedData[i]);
                     corAnomalyGraphData.push(corSelectedData[i]);
                 }
             });
 
+            // draw the anomaly ranges on the graph for selected feature, colored in red
             graph.config.data.datasets.unshift({
                 label: 'anomalies',
                 backgroundColor: 'rgb(255,0,0)',
@@ -163,6 +189,7 @@ function updateSelectedAnomalies() {
             });
             graph.update();
 
+            // draw the anomaly ranges on the graph for the correlative feature
             corGraph.config.data.datasets.unshift({
                 label: 'anomalies',
                 backgroundColor: 'rgb(255,0,0)',
@@ -173,23 +200,35 @@ function updateSelectedAnomalies() {
             });
             corGraph.update();
         } else {
+            // if there is no correlative feature, remove the anomalies column (no anomalies exist)
             $("#anomaliesCol").remove();
         }
     }
+    // update the anomalies table html with the anomaly ranges
     $("#anomaliesRows").html(s);
 }
 
+// function for highlighting the anomalies in the data table and list of features
 function updateTableAnomalies() {
+    // get list of attributes
     let attrs = Object.keys(loadedDetectData);
 
+    // iterate the features in the anomalies
     Object.keys(anomalyData.anomalies).forEach(key => {
+        // get name of correlative feature
         let corAttr = anomalyData.reason[key];
+        // if a correlative feature exists
         if (corAttr != undefined) {
+            // find indexes for the feature and its correlative feature in the attributes list
             let keyindex = attrs.findIndex((attr) => attr == key);
             let corKeyIndex = attrs.findIndex((attr) => attr == corAttr);
+            // if there are any anomalies
             if (anomalyData.anomalies[key].length > 0)
+                // color the feature in the features selection list in red
                 $("#select-" + key).css("background-color", "#dc3545");
+            // iterate the ranges of anomalies
             anomalyData.anomalies[key].forEach(range => {
+                // color all table cells of the anomalies in red
                 for (let i = range[0]; i < range[1]; i++) {
                     $(table.cell(i, keyindex + 1).node()).css("background-color", "#dc3545");
                     $(table.cell(i, corKeyIndex + 1).node()).css("background-color", "#dc3545");
@@ -199,6 +238,7 @@ function updateTableAnomalies() {
     });
 }
 
+// function for reading a CSV file into loadedData
 async function readFile(file, loadedData) {
     let text = await file.text();
     let data = text.split(/\r\n|\r|\n/);
@@ -220,42 +260,48 @@ async function readFile(file, loadedData) {
     }
 };
 
+// events for selecting a model type
 $("#regSelect").click(() => {
     modelType = 'regression';
 });
-
 $("#hybridSelect").click(() => {
     modelType = 'hybrid';
 });
 
+// event for clicking the train button
 $("#trainBtn").click(() => {
+    // check that a model type was selected
     if (modelType == undefined) {
         alertify.error("no model type selected. Please choose one.");
         return;
     }
+    // check that a train file was loaded
     if (loadedTrainFile == undefined) {
         alertify.error("no file loaded. Please upload a CSV file.");
         return;
     }
     let loadedTrainData = {};
+    // read the loaded file into loadedTrainData
     readFile(loadedTrainFile, loadedTrainData).then(() => {
 
         let body = { train_data: loadedTrainData };
-
+        // send a HTTP POST request for uploading a new model
         $.ajax({
             url: '/api/model?model_type=' + modelType,
             type: 'POST',
             dataType: 'json',
             contentType: 'application/json',
             success: (model) => {
-
+                // if request was succesful add the model to the table and notify user
                 addModel(model);
                 alertify.success("model uploaded succesfully");
 
+                // check each 3 seconds if the model is ready using HTTP GET request
                 let interval;
                 interval = setInterval(() => {
                     $.getJSON("/api/model?model_id=" + model.model_id, data => {
                         if (data.status == 'ready') {
+                            // when the model is ready stop sending GET requests and update status in the table
                             clearInterval(interval);
                             $("#status" + data.model_id).html("ready");
                         }
@@ -263,88 +309,118 @@ $("#trainBtn").click(() => {
                 }, 3000);
             },
             error: () => {
+                // notify if request failed
                 alertify.error("error in uploading model");
             },
             data: JSON.stringify(body)
         });
     });
+    // reset label for file upload
     $("#trainFileLabel").html("<strong>Choose a train file</strong><span> or drag it here.</span></label>")
 });
 
+// event for clicking the detect button
 $("#detectBtn").click(() => {
 
+    // check if a model was selected
     if (selectedModelID == undefined) {
-        alertify.error("no models loaded. Please upload a train file.");
+        alertify.error("no model selected. Please upload and select a model.");
         return;
     }
+    // check that the selected model isn't still pending
     if ($("#status" + selectedModelID).html() == "pending") {
         alertify.notify("selected model is pending. Please wait for it to be ready.");
         return;
     }
+    // check that a detect file is loaded
     if (loadedDetectFile == undefined) {
         alertify.error("no detection file loaded.");
         return;
     }
 
     loadedDetectData = {};
+    // read the data from the detect file to loadedDetectData
     readFile(loadedDetectFile, loadedDetectData).then(() => {
 
         let body = { predict_data: loadedDetectData };
 
+        // set selected feature to the first attribute from the file
         selectedFeature = Object.keys(loadedDetectData)[0];
+        // update the list of selections
         updateSelections();
+        // change selection in the html select element
         $("#featuresSelect").val(selectedFeature);
+        // make the select element visible
         $("#featuresSelect").css('visibility', 'visible');
 
+        // create the data table for currently loaded data
         createTable();
+        // update the graph for the data
         updateGraph();
 
+        // send a HTTP POST request to detect anomalies
         $.ajax({
             url: '/api/anomaly?model_id=' + selectedModelID,
             type: 'POST',
             dataType: 'json',
             contentType: 'application/json',
             success: (data) => {
+                // if request was succesful, save the anomalies
                 anomalyData = data;
+                // update the correlative graph
                 updateCorGraph();
+                // update the anomalies in the table
                 updateTableAnomalies();
+                // update anomalies in the graph and anomalies table for selected feature
                 updateSelectedAnomalies();
+                // notify user the anomalies were recieved
                 alertify.success("recieved anomaly report.");
             },
             error: () => {
+                // notify if the request failed
                 alertify.error("error in uploading detect file.");
             },
             data: JSON.stringify(body)
         });
     });
+    // reset the label for file uploading
     $("#detectFileLabel").html("<strong>Choose a detect file</strong><span> or drag it here.</span></label>")
 });
 
+// event for feature selection change
 $("#featuresSelect").change(() => {
+    // set selected feature from the select element
     selectedFeature = $("#featuresSelect").get(0).value;
-    updateGraph();
+    // update graph for newly selected feature
+    updateGraph();    
     $("#graph-card-body").addClass("scrollable");
+    // update graph for correlative feature
     updateCorGraph();
+    // update the anomalies on graph and anomalies table
     updateSelectedAnomalies();
 });
 
-$("#trainFileInput").change((event) => {
+// event for changing the loaded train file
+$("#trainFileInput").change((event) => {    
     if (event.target.files.length == 0)
         return;
+    // get name of loaded file
     loadedTrainFile = event.target.files[0];
     // change label to show file name
     $("#trainFileLabel").text(loadedTrainFile.name);
 });
 
+// event for changing the loaded detect file
 $("#detectFileInput").change((event) => {
     if (event.target.files.length == 0)
         return;
+    // get name of loaded file
     loadedDetectFile = event.target.files[0];
     // change label to show file name
     $("#detectFileLabel").text(loadedDetectFile.name);
 });
 
-// fileBox for train dropArea
+// event for dragging a file over the train drop-area
 $("#trainDropArea").on('dragover', (event) => {
     // prevent any unwanted behaviors for the assigned events across browsers.
     event.stopPropagation();
@@ -371,7 +447,7 @@ $("#trainDropArea").on('dragover', (event) => {
     }
 });
 
-// fileBox for detect dropArea
+// event for dragging a file over the detect drop-area
 $("#detectDropArea").on('dragover', (event) => {
     event.stopPropagation();
     event.preventDefault();
@@ -397,23 +473,27 @@ $("#detectDropArea").on('dragover', (event) => {
     }
 });
 
+// get the list of all models from the server and add each one to the models table
 $.getJSON("/api/models", data => {
     data.forEach(model => { addModel(model); });
 });
 
+
 var timesteps;
-
-
+// function for updating the graph from loaded data
 function updateGraph() {
 
+    // delete any previous graph
     if (graph != undefined)
         graph.destroy();
 
     timesteps = [];
+    // list of timesteps from the file
     for (let i = 0; i < loadedDetectData[selectedFeature].length; i++) {
         timesteps.push(i);
     }
 
+    // data for the graph
     let data = {
         data: timesteps,
         labels: timesteps,
@@ -427,6 +507,7 @@ function updateGraph() {
         }]
     };
 
+    // min/max values for the X/Y axes
     let miny = Math.min.apply(Math, loadedDetectData[selectedFeature]);
     let maxy = Math.max.apply(Math, loadedDetectData[selectedFeature]);
     if (miny == maxy) {
@@ -434,6 +515,7 @@ function updateGraph() {
         miny -= 5;
     }
 
+    // different configurations for the graph
     let config = {
         type: 'line',
         data,
@@ -468,21 +550,25 @@ function updateGraph() {
         }
     };
 
+    // update the graph from new configurations
     graph = new Chart(document.getElementById('graph'), config);
 
 }
 
-
-
+// function for updating the graph for the correlative feature if it exists
 function updateCorGraph() {
 
+    // delete any previous graph
     if (corGraph != undefined)
         corGraph.destroy();
 
+    // get name of correlative feature
     let corFeature = anomalyData.reason[selectedFeature];
 
+    // if a correlative feature exists
     if (corFeature != undefined) {
 
+        // data for the graph
         let data = {
             data: timesteps,
             labels: timesteps,
@@ -496,6 +582,7 @@ function updateCorGraph() {
             }]
         };
 
+        // min/max values for the X/Y axes
         let miny = Math.min.apply(Math, loadedDetectData[corFeature]);
         let maxy = Math.max.apply(Math, loadedDetectData[corFeature]);
         if (miny == maxy) {
@@ -503,6 +590,7 @@ function updateCorGraph() {
             maxy -= 5;
         }
 
+        // different configurations for the graph
         let config = {
             type: 'line',
             data,
@@ -537,6 +625,7 @@ function updateCorGraph() {
             }
         };
 
+        // update the correlative graph
         corGraph = new Chart(document.getElementById('corGraph'), config);
     }
 }
